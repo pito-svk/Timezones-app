e452ea773de9e8fbc4cd91c44c2731f20ab11a30
Peter Parada <parad.peter@gmail.com>
2017-06-02 00:22:22 +0100

    Apply standard --fix

--- public/timezone.js
+++ public/timezone.js
@@ -1,130 +1,127 @@
-'use strict';
+'use strict'
 
 var app = angular.module('timezoneApp', [])
 
 // Creates http request and returns UTC offset
-.factory('GetOffset', function($q, $http) {
-    var getOffset = function(city) {
-        var deferred = $q.defer();
-        var response = {};
+.factory('GetOffset', function ($q, $http) {
+  var getOffset = function (city) {
+    var deferred = $q.defer()
+    var response = {}
 
-        return $http.get('/getoffset/' + city);
-    };
-    return getOffset;
+    return $http.get('/getoffset/' + city)
+  }
+  return getOffset
 })
 
-.controller('TimeCtrl', function($scope, $timeout, GetOffset) {
-
+.controller('TimeCtrl', function ($scope, $timeout, GetOffset) {
     // Define static variables
-    var black = '#000000';
-    var gray = '#444444';
-    var tickInterval = 1000;
+  var black = '#000000'
+  var gray = '#444444'
+  var tickInterval = 1000
 
     // Define global variables
-    $scope.delimeterColor = black;
-    $scope.firstGray = false;
+  $scope.delimeterColor = black
+  $scope.firstGray = false
 
-    $scope.city = { input: 'New York', result: 'New York' };
-    $scope.time = { hours:'.', delimeter:'.', minutes:'.' };
+  $scope.city = { input: 'New York', result: 'New York' }
+  $scope.time = { hours: '.', delimeter: '.', minutes: '.' }
 
-    $scope.offset = null;
+  $scope.offset = null
 
     // Call when user change the city
-    $scope.changeCity = function() {
+  $scope.changeCity = function () {
+    $scope.time = { hours: '.', delimeter: '.', minutes: '.' }
+    $scope.delimeterColor = black
+    $scope.firstGray = false
 
-        $scope.time = { hours:'.', delimeter:'.', minutes:'.' };
-        $scope.delimeterColor = black;
-        $scope.firstGray = false;
-
-        GetOffset($scope.city.input)
-          .then(function success(response) {
-            var data = response.data;
+    GetOffset($scope.city.input)
+          .then(function success (response) {
+            var data = response.data
 
             if (data.status === 'cityNotFound') {
-                $scope.city.input = $scope.city.result;
+              $scope.city.input = $scope.city.result
             } else {
-                $scope.city.result = $scope.city.input;
+              $scope.city.result = $scope.city.input
 
-                var offset = data.offset;
-                var cityName = data.cityName;
+              var offset = data.offset
+              var cityName = data.cityName
 
-                $scope.offset = offset;
+              $scope.offset = offset
 
-                $scope.city.input = cityName;
-                $scope.city.result = cityName;
+              $scope.city.input = cityName
+              $scope.city.result = cityName
             };
-        });
-    };
+          })
+  }
 
-    $scope.changeCity();
+  $scope.changeCity()
 
     // Get current ISO time
-    var getISOTime = function() {
-        var localeDate = new Date();
-        var ISODate = new Date( localeDate.getTime() + (localeDate.getTimezoneOffset() * 60000));
-        return {
-            hours: ISODate.getHours(),
-            minutes: ISODate.getMinutes()
-        };
-    };
+  var getISOTime = function () {
+    var localeDate = new Date()
+    var ISODate = new Date(localeDate.getTime() + (localeDate.getTimezoneOffset() * 60000))
+    return {
+      hours: ISODate.getHours(),
+      minutes: ISODate.getMinutes()
+    }
+  }
 
     // Update time every second
-    var getTime = function() {
+  var getTime = function () {
+    var ISOTime = getISOTime()
+    var ISOHours = ISOTime.hours
+    var ISOMinutes = ISOTime.minutes
+    var ISOPeriod = ISOHours < 12 ? 'AM' : 'PM'
 
-        var ISOTime = getISOTime();
-        var ISOHours = ISOTime.hours;
-        var ISOMinutes = ISOTime.minutes;
-        var ISOPeriod = ISOHours < 12 ? 'AM' : 'PM';
+    var hours = ISOHours + ($scope.offset | 0)
+    var minutes = ISOMinutes + (($scope.offset % 1) * (60))
 
-        var hours = ISOHours + ($scope.offset | 0);
-        var minutes = ISOMinutes + (($scope.offset % 1) * (60));
+    var period = (hours >= 0 && hours < 12) ? 'AM' : 'PM'
 
-        var period = (hours >= 0 && hours < 12) ? 'AM' : 'PM';
-        
-        if (hours <= 0) {
-            hours += 12;
-        } else if (hours > 12) {
-            hours -= 12;
-        };
-
-        if (minutes >= 60) {
-            minutes -= 60;
-            hours += 1;
-        };
-
-        $scope.time.hours = hours;
-        $scope.time.delimeter = ':';
-        $scope.time.minutes = minutes;
-        $scope.time.period = period;
-
-        $timeout(getTime, tickInterval);
-
-        if ($scope.firstGray && $scope.delimeterColor == black) {
-            $scope.delimeterColor = gray;
-        } else {
-            $scope.delimeterColor = black;
-            $scope.firstGray = true;
-        };
+    if (hours <= 0) {
+      hours += 12
+    } else if (hours > 12) {
+      hours -= 12
     };
-    $timeout(getTime, tickInterval);
+
+    if (minutes >= 60) {
+      minutes -= 60
+      hours += 1
+    };
+
+    $scope.time.hours = hours
+    $scope.time.delimeter = ':'
+    $scope.time.minutes = minutes
+    $scope.time.period = period
+
+    $timeout(getTime, tickInterval)
+
+    if ($scope.firstGray && $scope.delimeterColor == black) {
+      $scope.delimeterColor = gray
+    } else {
+      $scope.delimeterColor = black
+      $scope.firstGray = true
+    };
+  }
+  $timeout(getTime, tickInterval)
 })
 
 // Call event when pressing Enter
 .directive('ngEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if(event.which === 13) {
-                scope.$apply(function (){
-                    scope.$eval(attrs.ngEnter);
-                });
-                event.preventDefault();
-            }
-        });
-    };
+  return function (scope, element, attrs) {
+    element.bind('keydown keypress', function (event) {
+      if (event.which === 13) {
+        scope.$apply(function () {
+          scope.$eval(attrs.ngEnter)
+        })
+        event.preventDefault()
+      }
+    })
+  }
 })
 
 .filter('time', function () {
-    return function(n){
-        return (n < 10) ? '0' + n : n
-    }
-});
+  return function (n) {
+    return (n < 10) ? '0' + n : n
+  }
+})
