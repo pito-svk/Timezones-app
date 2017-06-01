835ed1a3a7cc8a9ddc9124eb79b57fb42c10ba61
Peter Parada <parad.peter@gmail.com>
2017-06-02 00:21:50 +0100

    Start refactor server

--- server.js
+++ server.js
@@ -1,77 +1,65 @@
-'use strict';
+const express = require('express')
+const requestify = require('requestify')
 
-var express = require('express'),
-    requestify = require('requestify');
+const app = express()
 
-// Setup server
-var app = express();
+app.set('views', `${__dirname}/public`)
+app.engine('html', require('ejs').renderFile)
+app.use(express.static(`${__dirname}/public`))
 
-// Configure views
-app.set('views', __dirname + '/public');
-app.engine('html', require('ejs').renderFile);
-app.use(express.static(__dirname + '/public'));
+const APIs = {
+  googleMaps: city => {
+    return `http://maps.googleapis.com/maps/api/geocode/json?address=${city}&sensor=false`
+  },
+  geoNames: (lat, lng) => {
+    const username = 'YOUR_USERNAME'
+    const host = 'http://ws.geonames.org'
+    const path = '/timezoneJSON?lat=' + lat + '&lng=' + lng + '&username=' + username
+    return host + path
+  }
+}
 
-// Configure third-party APIs
-var APIs = {
-    googleMaps: function(city) {
-        var host = 'http://maps.googleapis.com';
-        var path = '/maps/api/geocode/json?address=' + city + '&sensor=false';
-        return host + path;
-    },
-    geoNames: function(lat, lng) {
-        var username = 'YOUR_USERNAME';
-        var host = 'http://ws.geonames.org';
-        var path = '/timezoneJSON?lat=' + lat + '&lng=' + lng + '&username=' + username;
-        return host + path;
-    }
-};
+app.get('/', (req, res) => {
+  res.render('timezone.html')
+})
 
-// Configure routes
-app.get('/', function(req, res) {
-    res.render('timezone.html');
-});
+app.get('/getoffset/:city', function (req, res) {
+  res.setHeader('Content-Type', 'application/json')
 
-app.get('/getoffset/:city', function(req, res) {
+  const googleMapsURL = APIs.googleMaps(req.params.city)
 
-    // Send response as JSON
-    res.setHeader('Content-Type', 'application/json');
+  requestify.get(googleMapsURL)
+      .then(function (response) {
+        var googleMapsData = response.getBody()
 
-    var googleMapsURL = APIs.googleMaps(req.params.city);
+        if (googleMapsData.status === 'OK') {
+          var firstRes = googleMapsData.results[0]
+          var coords = firstRes.geometry.location
 
-    // Send request
-    requestify.get(googleMapsURL)
-      .then(function(response) {
-          var googleMapsData = response.getBody();
+          var lat = coords.lat
+          var lng = coords.lng
+          var cityName = firstRes.address_components[0].long_name
 
-          if (googleMapsData.status === 'OK') {
-            var firstRes = googleMapsData.results[0];
-            var coords = firstRes.geometry.location;
+          var geoNamesURL = APIs.geoNames(lat, lng)
 
-            var lat = coords.lat;
-            var lng = coords.lng;
-            var cityName = firstRes.address_components[0].long_name;
+          requestify.get(geoNamesURL)
+              .then(function (response) {
+                var geoNamesData = response.getBody()
 
-            var geoNamesURL = APIs.geoNames(lat, lng);
+                var offset = geoNamesData.gmtOffset
 
-            requestify.get(geoNamesURL)
-              .then(function(response) {
-                var geoNamesData = response.getBody();
-
-                var offset = geoNamesData.gmtOffset;
-
-                res.send(JSON.stringify({ status: 'success', offset: offset, cityName: cityName }));
-              });
-
-          } else {
-              res.send(JSON.stringify({ status: 'cityNotFound' }));
-          };
+                res.send(JSON.stringify({ status: 'success', offset: offset, cityName: cityName }))
+              })
+        } else {
+          res.send(JSON.stringify({ status: 'cityNotFound' }))
+        };
       }
-    );
-});
+    )
+})
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000
 
 // Listen to server
 app.listen(port, function () {
-    console.log('Express server listening on %d, in %s mode', port, app.get('env'));
-});
+  console.log('Express server listening on %d, in %s mode', port, app.get('env'))
+})
